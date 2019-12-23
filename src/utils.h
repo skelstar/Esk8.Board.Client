f43d085174734f0336d2a5dc15986bb38c885a6c
Sean <skelstar@gmail.com>
2019-12-24 09:42:24 +1300

    Added reset reason

--- src/utils.h
+++ src/utils.h
@@ -1,4 +1,4 @@
-
+#include <rom/rtc.h>
 
 uint8_t getBatteryPercentage(float voltage) {
   float voltsLeft = voltage - BATTERY_VOLTAGE_CUTOFF_END;
@@ -39,4 +39,51 @@ void deepSleep()
   // u8g2.setPowerSave(1);
   delay(500);
   pureDeepSleep();
+}
+
+// enum ResetReason
+// {
+//     POWERON_RESET,          /**<1, Vbat power on reset*/
+//     DONT_KNOW,
+//     SW_RESET,               /**<3, Software reset digital core*/
+//     OWDT_RESET,             /**<4, Legacy watch dog reset digital core*/
+//     DEEPSLEEP_RESET,        /**<5, Deep Sleep reset digital core*/
+//     SDIO_RESET,             /**<6, Reset by SLC module, reset digital core*/
+//     TG0WDT_SYS_RESET,       /**<7, Timer Group0 Watch dog reset digital core*/
+//     TG1WDT_SYS_RESET,       /**<8, Timer Group1 Watch dog reset digital core*/
+//     RTCWDT_SYS_RESET,       /**<9, RTC Watch dog Reset digital core*/
+//     INTRUSION_RESET,       /**<10, Instrusion tested to reset CPU*/
+//     TGWDT_CPU_RESET,       /**<11, Time Group reset CPU*/
+//     SW_CPU_RESET,          /**<12, Software reset CPU*/
+//     RTCWDT_CPU_RESET,      /**<13, RTC Watch dog Reset CPU*/
+//     EXT_CPU_RESET,         /**<14, for APP CPU, reseted by PRO CPU*/
+//     RTCWDT_BROWN_OUT_RESET, /**<15, Reset when the vdd voltage is not stable*/
+//     RTCWDT_RTC_RESET,      /**<16, RTC Watch dog reset digital core and rtc module*/
+//     NO_MEAN,
+// };
+
+void print_reset_reason(uint8_t cpu_core)
+{
+  RESET_REASON reason = rtc_get_reset_reason(cpu_core);
+  switch ( reason )
+  {
+    case POWERON_RESET : Serial.printf("POWERON_RESET");break;          /**<1, Vbat power on reset*/
+    case 2: Serial.println("DONT_KNOW");break;
+    case SW_RESET : Serial.printf("SW_RESET");break;               /**<3, Software reset digital core*/
+    case OWDT_RESET : Serial.printf("OWDT_RESET");break;             /**<4, Legacy watch dog reset digital core*/
+    case DEEPSLEEP_RESET : Serial.printf("DEEPSLEEP_RESET");break;        /**<5, Deep Sleep reset digital core*/
+    case SDIO_RESET : Serial.printf("SDIO_RESET");break;             /**<6, Reset by SLC module, reset digital core*/
+    case TG0WDT_SYS_RESET : Serial.printf("TG0WDT_SYS_RESET");break;       /**<7, Timer Group0 Watch dog reset digital core*/
+    case TG1WDT_SYS_RESET : Serial.printf("TG1WDT_SYS_RESET");break;       /**<8, Timer Group1 Watch dog reset digital core*/
+    case RTCWDT_SYS_RESET : Serial.printf("RTCWDT_SYS_RESET");break;       /**<9, RTC Watch dog Reset digital core*/
+    case INTRUSION_RESET : Serial.printf("INTRUSION_RESET");break;       /**<10, Instrusion tested to reset CPU*/
+    case TGWDT_CPU_RESET : Serial.printf("TGWDT_CPU_RESET");break;       /**<11, Time Group reset CPU*/
+    case SW_CPU_RESET : Serial.printf("SW_CPU_RESET");break;          /**<12, Software reset CPU*/
+    case RTCWDT_CPU_RESET : Serial.printf("RTCWDT_CPU_RESET");break;      /**<13, RTC Watch dog Reset CPU*/
+    case EXT_CPU_RESET : Serial.printf("EXT_CPU_RESET");break;         /**<14, for APP CPU, reseted by PRO CPU*/
+    case RTCWDT_BROWN_OUT_RESET : Serial.printf("RTCWDT_BROWN_OUT_RESET");break;/**<15, Reset when the vdd voltage is not stable*/
+    case RTCWDT_RTC_RESET : Serial.printf("RTCWDT_RTC_RESET");break;      /**<16, RTC Watch dog reset digital core and rtc module*/
+    
+    default : Serial.printf("NO_MEAN");
+  }
 }
